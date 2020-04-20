dc2ca51672c20898c74943640bcc268423cd3cdc
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-04-20 13:12:12 +0200

    Initail commit: set up empty spring project

---
+++ src/main/java/com/coodcool/icook/IcookApplication.java
@@ -0,0 +1,13 @@
+package com.coodcool.icook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coodcool/icook/IcookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coodcool.icook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
