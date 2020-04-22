b42091f50a505ee6ada49ff16dc641e106d6d95e
Rajnai Máté <ramate.rajnai@gmail.com>
2020-04-22 11:20:26 +0200

    Create API endpoints and related actions

---
+++ src/main/java/com/coodcool/icook/controller/CommentController.java
@@ -0,0 +1,32 @@
+package com.coodcool.icook.controller;
+
+import com.coodcool.icook.dao.CommentDao;
+import com.coodcool.icook.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/recipe")
+public class CommentController {
+
+    @Autowired
+    private CommentDao commentDao;
+
+
+
+    @GetMapping("/{id}/comments")
+    public List<Comment> getCommentsBy(@PathVariable("id") String id) {
+        return this.commentDao.getCommentsBy(id);
+    }
+
+    @PostMapping("/{id}/comments")
+    public List<Comment> addComment(@RequestBody Comment comment, @PathVariable("id") String id) {
+        comment.setSubmissionTime(LocalDateTime.now());
+        this.commentDao.addComment(comment);
+        return this.commentDao.getCommentsBy(id);
+    }
+
+}
