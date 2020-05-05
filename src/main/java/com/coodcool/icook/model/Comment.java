cb06637cae8dd9af56f3abef698329bcdb353f64
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-05 15:50:21 +0200

    Complete comment POJO class with JPA annotations

--- src/main/java/com/coodcool/icook/model/Comment.java
+++ src/main/java/com/coodcool/icook/model/Comment.java
@@ -1,16 +1,31 @@
 package com.coodcool.icook.model;
 
 import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Comment {
 
-    private String id;
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String content;
+
+    @Column(nullable = false)
     private LocalDateTime submissionTime;
+
+    @Column(nullable = false)
     private String recipeId;
+
+//    private User user;
 
 }
