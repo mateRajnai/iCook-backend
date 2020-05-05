484b4740d4f84115a2411fc0842b2cd3210e7a68
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-05-05 14:30:03 +0200

    Add FavoriteRecipeRepository

--- src/main/java/com/coodcool/icook/model/FavoriteRecipe.java
+++ src/main/java/com/coodcool/icook/model/FavoriteRecipe.java
@@ -1,9 +1,18 @@
 package com.coodcool.icook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class FavoriteRecipe {
     @Id
