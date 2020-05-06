796f737338d07eefd3be4f63200ad79edc524d3a
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-05-06 21:02:57 +0200

    Add address test class and first method implemettation

--- src/main/java/com/coodcool/icook/dao/repository/FavoriteRecipeRepository.java
+++ src/main/java/com/coodcool/icook/dao/repository/FavoriteRecipeRepository.java
@@ -4,6 +4,8 @@ import com.coodcool.icook.dao.FavoriteRecipeIdsDao;
 import com.coodcool.icook.model.FavoriteRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FavoriteRecipeRepository extends JpaRepository<FavoriteRecipe, Long> {
 
 }
