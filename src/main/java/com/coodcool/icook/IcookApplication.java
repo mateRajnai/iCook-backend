4d28d347d654950fe95255b29aac4e8ab42d6183
RolandOver9000 <csosz.roland2@gmail.com>
2020-05-06 11:30:19 +0200

    replaced personal note classes

--- src/main/java/com/coodcool/icook/IcookApplication.java
+++ src/main/java/com/coodcool/icook/IcookApplication.java
@@ -1,13 +1,38 @@
 package com.coodcool.icook;
 
+import com.coodcool.icook.dao.repository.FavoriteRecipeRepository;
+import com.coodcool.icook.dao.repository.PersonalNoteRepository;
+import com.coodcool.icook.model.FavoriteRecipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class IcookApplication {
+
+    @Autowired
+    FavoriteRecipeRepository favoriteRecipeRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(IcookApplication.class, args);
     }
 
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+            FavoriteRecipe favoriteRecipe = FavoriteRecipe.builder()
+                    .recipeId("1")
+                    .bookmarkedTime(10)
+                    .build();
+
+            favoriteRecipeRepository.save(favoriteRecipe);
+        };
+    }
 }
--- src/test/java/com/coodcool/icook/dao/repository/PersonalNoteRepositoryTest.java
+++ src/test/java/com/coodcool/icook/dao/repository/PersonalNoteRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.coodcool.icook.dao.repository;
 
 import com.coodcool.icook.model.PersonalNote;
-import com.coodcool.icook.dao.repository.PersonalNoteRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
