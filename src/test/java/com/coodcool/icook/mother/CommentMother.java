7429039f659c69c976af4b858a5f5da4cd2ac212
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-06 15:20:47 +0200

    Extract recipeId into final variable

--- src/test/java/com/coodcool/icook/mother/CommentMother.java
+++ src/test/java/com/coodcool/icook/mother/CommentMother.java
@@ -6,17 +6,19 @@ import java.time.LocalDateTime;
 
 public class CommentMother {
 
+    private static final String RECIPE_ID = "http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_233567d117b63a90fd90dabbcd4e4f39";
+
     public static Comment.CommentBuilder completeWithoutId() {
         return Comment.builder()
                 .content("Test comment")
-                .recipeId("http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_233567d117b63a90fd90dabbcd4e4f39")
+                .recipeId(RECIPE_ID)
                 .submissionTime(LocalDateTime.now());
     }
 
     public static Comment.CommentBuilder completeWithoutIdAndSubmissionTime() {
         return Comment.builder()
                 .content("Test comment")
-                .recipeId("http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_233567d117b63a90fd90dabbcd4e4f39");
+                .recipeId(RECIPE_ID);
     }
 
     public static Comment.CommentBuilder completeWithoutIdAndRecipeId() {
