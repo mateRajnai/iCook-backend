e004fa2b3605040b108bfe6d94f0b67758c4b912
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-06 14:54:52 +0200

    Add content to comment builder

--- src/test/java/com/coodcool/icook/mother/CommentMother.java
+++ src/test/java/com/coodcool/icook/mother/CommentMother.java
@@ -8,6 +8,7 @@ public class CommentMother {
 
     public static Comment.CommentBuilder completeWithoutId() {
         return Comment.builder()
+                .content("Test comment")
                 .recipeId("http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_233567d117b63a90fd90dabbcd4e4f39")
                 .submissionTime(LocalDateTime.now());
     }
