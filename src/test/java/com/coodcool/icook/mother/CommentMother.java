7137f2216273aa7fb8be50ab8d3dfb084af789a9
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-06 16:19:29 +0200

    Extract content into final variable in CommentMother

--- src/test/java/com/coodcool/icook/mother/CommentMother.java
+++ src/test/java/com/coodcool/icook/mother/CommentMother.java
@@ -7,23 +7,24 @@ import java.time.LocalDateTime;
 public class CommentMother {
 
     private static final String RECIPE_ID = "http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_233567d117b63a90fd90dabbcd4e4f39";
+    private static final String CONTENT = "Test comment";
 
     public static Comment.CommentBuilder completeWithoutId() {
         return Comment.builder()
-                .content("Test comment")
+                .content(CONTENT)
                 .recipeId(RECIPE_ID)
                 .submissionTime(LocalDateTime.now());
     }
 
     public static Comment.CommentBuilder completeWithoutIdAndSubmissionTime() {
         return Comment.builder()
-                .content("Test comment")
+                .content(CONTENT)
                 .recipeId(RECIPE_ID);
     }
 
     public static Comment.CommentBuilder completeWithoutIdAndRecipeId() {
         return Comment.builder()
-                .content("Test comment")
+                .content(CONTENT)
                 .submissionTime(LocalDateTime.now());
     }
 
