2bb4cbd42564ea8620c7a12d52b04b3e3e8f75de
RolandOver9000 <47983861+RolandOver9000@users.noreply.github.com>
2020-05-07 18:55:52 +0200

    deleted unused imports

--- src/main/java/com/coodcool/icook/IcookApplication.java
+++ src/main/java/com/coodcool/icook/IcookApplication.java
@@ -1,14 +1,8 @@
 package com.coodcool.icook;
 
-import com.coodcool.icook.controller.PersonalNoteController;
-import com.coodcool.icook.dao.repository.PersonalNoteRepository;
-import com.coodcool.icook.model.PersonalNote;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class IcookApplication {
