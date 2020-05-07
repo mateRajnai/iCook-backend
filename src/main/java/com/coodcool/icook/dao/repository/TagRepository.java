5f28fef663de405fe3c8eafcdf07e522a76a568b
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-07 13:21:57 +0200

    Create tag jpa repository

---
+++ src/main/java/com/coodcool/icook/dao/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.coodcool.icook.dao.repository;
+
+import com.coodcool.icook.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+
+}
