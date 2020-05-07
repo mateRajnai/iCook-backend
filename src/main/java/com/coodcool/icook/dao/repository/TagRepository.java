5b1141cd7778df50ac04f543712f9b432ebb22d5
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-07 15:27:42 +0200

    Create repo method of finding tags by user

--- src/main/java/com/coodcool/icook/dao/repository/TagRepository.java
+++ src/main/java/com/coodcool/icook/dao/repository/TagRepository.java
@@ -1,9 +1,22 @@
 package com.coodcool.icook.dao.repository;
 
+import com.coodcool.icook.model.FavoriteRecipe;
 import com.coodcool.icook.model.Tag;
+import com.coodcool.icook.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+
+
+    @Query("SELECT tag FROM Tag tag WHERE tag.user.id = :userId")
+    List<Tag> findAllByUserId(@Param("userId") Long userId);
+
 
 
 }
