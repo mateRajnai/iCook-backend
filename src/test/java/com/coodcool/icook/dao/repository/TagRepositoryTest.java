1014628c05d7848d89de1946787608dba8d005cb
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-07 15:28:12 +0200

    test finding tags by userid

--- src/test/java/com/coodcool/icook/dao/repository/TagRepositoryTest.java
+++ src/test/java/com/coodcool/icook/dao/repository/TagRepositoryTest.java
@@ -6,6 +6,7 @@ import com.coodcool.icook.model.User;
 import com.coodcool.icook.mother.FavoriteRecipeMother;
 import com.coodcool.icook.mother.TagMother;
 import com.coodcool.icook.mother.UserMother;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,6 +28,9 @@ public class TagRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private FavoriteRecipeRepository favoriteRecipeRepository;
+
     @Test
     public void saveOneCompleteTag() {
         User user = UserMother.withoutAnyRelationsAndId().build();
@@ -45,5 +49,26 @@ public class TagRepositoryTest {
         assertThat(addedTag.getTaggedFavorites()).isNotEmpty();
 
     }
+
+
+
+    @Test
+    public void findAllTagByUserId() {
+        // methodba kiszervezni
+        User user = UserMother.withoutAnyRelationsAndId().build();
+        User user2 = UserMother.withoutAnyRelationsAndId().build();
+        Tag tag = TagMother.withoutIdAndWithCustom(user, null).build();
+        Tag tag2 = TagMother.withoutIdAndWithCustom(user2, null).tag("lunch").build();
+
+        user.setTags(Set.of(tag));
+        user2.setTags(Set.of(tag2));
+
+        userRepository.saveAll(Lists.newArrayList(user, user2));
+        List<Tag> tags = tagRepository.findAllByUserId(user.getId());
+
+        assertThat(tags).hasSize(1).containsExactly(tag);
+    }
+
+
 
 }
