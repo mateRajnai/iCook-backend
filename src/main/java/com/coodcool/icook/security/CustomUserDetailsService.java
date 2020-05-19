83a15c467bcece63f36ca5150f0aa2bbfface989
Rajnai Máté <ramate.rajnai@gmail.com>
2020-05-19 11:11:53 +0200

    CReate custom user details service

---
+++ src/main/java/com/coodcool/icook/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.coodcool.icook.security;
+
+import com.coodcool.icook.dao.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository users;
+
+    public CustomUserDetailsService(UserRepository users) {
+        this.users = users;
+    }
+
+    /**
+     * Loads the user from the DB and converts it to Spring Security's internal User object
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.coodcool.icook.model.User user = users.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username: " + username + " not found"));
+
+        return new User(user.getUserName(), user.getPassword(),
+                user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+    }
+}
