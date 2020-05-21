81fb45006b44a0fb8610c4ef7434c443dceed8f6
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-05-21 14:23:47 +0200

    Put user id into the jwt token

--- src/main/java/com/coodcool/icook/security/JwtTokenServices.java
+++ src/main/java/com/coodcool/icook/security/JwtTokenServices.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
@@ -29,16 +28,19 @@ public class JwtTokenServices {
 
     private final String rolesFieldName = "roles";
 
+    private final String userIdFieldName = "id";
+
     @PostConstruct
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
     // Creates a JWT token
-    public String createToken(String username, List<String> roles) {
+    public String createToken(String username, List<String> roles, String id) {
         // Add a custom field to the token
         Claims claims = Jwts.claims().setSubject(username);
         claims.put(rolesFieldName, roles);
+        claims.put(userIdFieldName, id);
 
         Date now = new Date();
         Date validity = new Date(now.getTime() + validityInMilliseconds);
