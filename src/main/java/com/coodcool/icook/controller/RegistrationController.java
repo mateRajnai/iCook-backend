b6281d360a565c2d845d97e8ef634dd99f5fed56
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-06-03 12:42:09 +0200

    Add email and username checking routes to regsitration controller and refactor passwordEncoder to a bean instead instantiating in controller's constructor

--- src/main/java/com/coodcool/icook/controller/RegistrationController.java
+++ src/main/java/com/coodcool/icook/controller/RegistrationController.java
@@ -2,24 +2,23 @@ package com.coodcool.icook.controller;
 
 import com.coodcool.icook.dao.repository.UserRepository;
 import com.coodcool.icook.model.User;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/signup")
 public class RegistrationController {
 
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
-
-    public RegistrationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("")
     public ResponseEntity<String> register(@RequestBody User user) {
@@ -32,5 +31,17 @@ public class RegistrationController {
             log.error(e.getMessage());
             return ResponseEntity.unprocessableEntity().build();
         }
+    }
+
+    @PostMapping("/check_username")
+    public ResponseEntity<Boolean> checkUsername(@RequestBody String username) {
+        Optional<User> user = userRepository.findByUserName(username);
+        return ResponseEntity.ok(user.isPresent());
+    }
+
+    @PostMapping("/check_email")
+    public ResponseEntity<Boolean> checkEmail(@RequestBody String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user.isPresent());
     }
 }
