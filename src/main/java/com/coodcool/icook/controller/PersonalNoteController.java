42b3cc070f73053fef2c9f346c4d489d31ef34ad
Rajnai Máté <ramate.rajnai@gmail.com>
2020-06-04 15:24:06 +0200

    Change return value to ResponseEntity in controller

--- src/main/java/com/coodcool/icook/controller/PersonalNoteController.java
+++ src/main/java/com/coodcool/icook/controller/PersonalNoteController.java
@@ -3,8 +3,10 @@ package com.coodcool.icook.controller;
 import com.coodcool.icook.model.PersonalNote;
 import com.coodcool.icook.service.PersonalNoteHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -15,13 +17,13 @@ public class PersonalNoteController {
     private final PersonalNoteHandler personalNoteHandler;
 
     @GetMapping("")
-    public List<PersonalNote> getPersonalNotes(@PathVariable("id") String id) {
-        return this.personalNoteHandler.getPersonalNoteByRecipeId(id);
+    public ResponseEntity<List<PersonalNote>> getPersonalNotes(@PathVariable("id") String id, HttpServletRequest request) {
+        return ResponseEntity.ok(this.personalNoteHandler.getPersonalNoteByRecipeId(id, request));
     }
 
     @PostMapping("")
-    public PersonalNote createPersonalNote(@RequestBody PersonalNote personalNote ) {
-        return this.personalNoteHandler.addPersonalNote(personalNote);
+    public ResponseEntity<PersonalNote> createPersonalNote(@RequestBody PersonalNote personalNote, HttpServletRequest request) {
+        return ResponseEntity.ok(this.personalNoteHandler.addPersonalNote(personalNote, request));
     }
 
 }
